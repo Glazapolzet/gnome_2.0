d5b94fc6ac9bea10995e1b2c0eb3b9b51e73f402
zuzuka28 <zuzuka28kavaii@gmail.com>
2023-07-24 14:14:49 +0300

    add svg support

--- src/components/Button/ButtonClose/ButtonClose.tsx
+++ src/components/Button/ButtonClose/ButtonClose.tsx
@@ -1,3 +1,4 @@
+import { ReactComponent as Icon } from '@/assets/icon/cross.svg';
 import React from 'react';
 import { ButtonClose as Button } from './styled';
 
@@ -8,9 +9,7 @@ export interface ButtonCloseProps {
 export const ButtonClose: React.FC<ButtonCloseProps> = ({onClick}) => {
     return (
         <Button onClick={onClick}>
-            <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1 1L11 11M1 11L11 1" stroke="#444444" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>
+             <Icon/>
         </Button>
     )
 }
